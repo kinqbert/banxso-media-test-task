2f977821470b19ec5c0bbb2b3b3a918cc7cd73e3
kinqbert <perehodko1010@gmail.com>
2024-08-08 14:44:23 +0300

    replaced rocket api url

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const ROCKETS_URL = "https://api.spacexdata.com/v4/dragons";
-const MOCK_ROCKETS_URL = "https://kinqbert.github.io/banxso-media-test-task/api/testrockets.json";
+const ROCKETS_URL = "https://api.spacexdata.com/v4/dragons";
+// const MOCK_ROCKETS_URL = "https://kinqbert.github.io/banxso-media-test-task/api/testrockets.json";
 const SERVER_URL = "https://kinqbert.github.io/banxso-media-test-task/api";
 
 export const rocketsApiClient = axios.create({
-  baseURL: MOCK_ROCKETS_URL,
+  baseURL: ROCKETS_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
